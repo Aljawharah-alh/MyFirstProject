9a2dcced38636d85bfac5581e0ee1cd006f389fa
Aljawharah Alhuqbani <aljwharahsaad785@gmail.com>
2026-09-14 22:19:45 +0300

    Update HelloWorld from clone

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Welcome to Git Branching!");
+        System.out.println("Welcome Aljawharah");
     }
 }
